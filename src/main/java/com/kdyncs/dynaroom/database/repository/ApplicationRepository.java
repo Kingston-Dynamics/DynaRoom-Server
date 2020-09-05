9d8d4a89747a0d25004ffe51255177b532dc8d16
Peter Rose <peterrose1994@gmail.com>
2020-09-05 17:49:59 -0400

    Applying license header

--- src/main/java/com/kdyncs/dynaroom/database/repository/ApplicationRepository.java
+++ src/main/java/com/kdyncs/dynaroom/database/repository/ApplicationRepository.java
@@ -1,11 +1,23 @@
-package com.kdyncs.dynaroom.database.repository;
+/*
+ * Copyright (C) 2020 Kingston Dynamics Inc.
+ * 
+ * This file is part of DynaRoom
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.kdyncs.dynaroom.database.schema.software.repository;
+package com.kdyncs.dynaroom.database.repository;
 //
 //import com.kdyncs.dynaroom.database.schema.software.model.ApplicationModel;
 //import java.util.List;
